a7a37ce062bc22c425738f09ab8c03c733ab9066
submonkey <hunt.if@gmail.com>
2013-08-05 12:38:40 +0700

    add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+cd ~/.vim
+
+echo 'set runtimepath+=~/.vim
+
+source ~/.vim/vimrcs/functions.vim
+source ~/.vim/vimrcs/config.vim
+source ~/.vim/vimrcs/bundles.vim
+source ~/.vim/vimrcs/maps.vim
+source ~/.vim/vimrcs/autocommands.vim' > ~/.vimrc
+
+echo 'vimrc succesfully installed'
